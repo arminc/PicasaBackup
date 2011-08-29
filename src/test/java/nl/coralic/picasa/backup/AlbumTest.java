3c549e96f331d0f10c2c3f95dd029f1f7c75fb82
Armin <armin@coralic.nl>
2011-08-29 17:48:26 +0200

    correct way of testing the albums

--- src/test/java/nl/coralic/picasa/backup/AlbumTest.java
+++ src/test/java/nl/coralic/picasa/backup/AlbumTest.java
@@ -1,33 +1,63 @@
 package nl.coralic.picasa.backup;
 
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import com.google.gdata.data.photos.AlbumEntry;
-import com.google.gdata.util.AuthenticationException;
 
-public class AlbumTest extends Initialize
-{	
-	private Picasa pwaBackup;
+public class AlbumTest
+{		
+	private static final String ALBUMID = "ID123456789";
+	Albums albums;
 	
 	@Before
-	public void initialize() throws AuthenticationException
+	public void initialize()
 	{
-		pwaBackup = new Picasa(USERNAME, PASSWORD);
+		albums = new Albums(createFakeAlbumList());
+	}
+	
+	private List<AlbumEntry> createFakeAlbumList()
+	{
+		List<AlbumEntry> albumList = new ArrayList<AlbumEntry>();
+		albumList.add(createFakeAlbumEntry());
+		return albumList;
+	}
+	
+	private AlbumEntry createFakeAlbumEntry()
+	{
+		AlbumEntry fakeAlbumEntry = mock(AlbumEntry.class);
+		when(fakeAlbumEntry.getId()).thenReturn("http://picasa......./album/"+ALBUMID);
+		return fakeAlbumEntry;
 	}
 	
 	@Test
-	public void fetchAll()
+	public void notNull()
 	{
-		List<AlbumEntry> albums = pwaBackup.fetchAlbums();
-		for (AlbumEntry album : albums) {
-		    System.out.println(album.getName());
-		}
-		assertNotSame(0, albums.size());
+		assertNotNull(albums);
 	}
+	
+	@Test
+	public void getSize()
+	{
+		assertSame(1, albums.size());
+	}
+	
+	@Test 
+	public void getAlbumId()
+	{
+		for(Album album : albums)
+		{
+			assertEquals(ALBUMID, album.getAlbumId());
+			return;
+		}
+		fail("Did not go in to for loop.");
+	}
+
 }
