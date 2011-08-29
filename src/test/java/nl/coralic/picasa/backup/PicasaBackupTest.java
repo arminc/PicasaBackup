d946fff5e6a8b9970e69ba4bd329387a1d845de9
Armin <armin@coralic.nl>
2011-08-29 17:50:12 +0200

    PicasaBackup application test

---
+++ src/test/java/nl/coralic/picasa/backup/PicasaBackupTest.java
@@ -0,0 +1,85 @@
+package nl.coralic.picasa.backup;
+
+import java.security.Permission;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class PicasaBackupTest
+{
+
+	@BeforeClass
+	public static void setUp() throws Exception
+	{
+		System.setSecurityManager(new NoExitSecurityManager());
+	}
+
+	@AfterClass
+	public static void tearDown() throws Exception
+	{
+		System.setSecurityManager(null);
+	}
+
+	@Test
+	public void checkArgumentsCorrect()
+	{
+		PWABackup.main(new String[]	{ "1", "2", "3" });
+	}
+
+	@Test
+	public void checkArgumentsWrong()
+	{
+		try
+		{
+			PWABackup.main(new String[]
+			{ "1" });
+		} catch (ExitException e)
+		{
+			assertEquals(0, e.status);
+		}
+	}
+	
+	@Test
+	public void checkCorrectFolder()
+	{
+		
+	}
+	
+	//These two classes needed for testing System.exit in the main function
+	protected static class ExitException extends SecurityException
+	{
+		private static final long serialVersionUID = -4307476468054559890L;
+		public final int status;
+
+		public ExitException(int status)
+		{
+			super("There is no escape!");
+			this.status = status;
+		}
+	}
+
+	private static class NoExitSecurityManager extends SecurityManager
+	{
+		@Override
+		public void checkPermission(Permission perm)
+		{
+			// allow anything.
+		}
+
+		@Override
+		public void checkPermission(Permission perm, Object context)
+		{
+			// allow anything.
+		}
+
+		@Override
+		public void checkExit(int status)
+		{
+			super.checkExit(status);
+			throw new ExitException(status);
+		}
+	}
+
+}
