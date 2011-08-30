5423d8fd3352d270778417b022c684586b4e13cd
Armin <armin@coralic.nl>
2011-08-30 15:56:06 +0200

    test for existence and creation of folders

--- src/main/java/nl/coralic/picasa/backup/FileHandler.java
+++ src/main/java/nl/coralic/picasa/backup/FileHandler.java
@@ -1,14 +1,27 @@
 package nl.coralic.picasa.backup;
 
 import java.io.File;
+import java.io.IOException;
 
 public class FileHandler
 {
 
-	public boolean folderExists(String folder)
+	public static boolean folderExists(String folder)
 	{
 		File file = new File(folder);
 		return file.exists();
+	}
+
+	public static boolean createFolder(String rootFolder, String newFolder)
+	{
+		File folderToCreate = new File(rootFolder + File.pathSeparator + newFolder);
+		try
+		{
+			return folderToCreate.createNewFile();
+		} catch (IOException e)
+		{
+			return false;
+		}
 	}
 
 }
