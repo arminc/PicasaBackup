8dbd5cb02caff5f2f584ca31921b0ae9c31d3bbd
Armin <armin@coralic.nl>
2011-08-31 15:27:48 +0200

    use commons-io for file handling

--- src/main/java/nl/coralic/picasa/backup/file/FileHandler.java
+++ src/main/java/nl/coralic/picasa/backup/file/FileHandler.java
@@ -3,25 +3,25 @@ package nl.coralic.picasa.backup.file;
 import java.io.File;
 import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+
 public class FileHandler
 {
-
 	public static boolean folderExists(String folder)
 	{
-		File file = new File(folder);
-		return file.exists();
+		return new File(folder).exists();
 	}
 
 	public static boolean createFolder(String rootFolder, String newFolder)
 	{
-		File folderToCreate = new File(rootFolder + File.pathSeparator + newFolder);
+		File folderToCreate = new File(rootFolder + File.separator + newFolder);
 		try
 		{
-			return folderToCreate.createNewFile();
+			FileUtils.forceMkdir(folderToCreate);
 		} catch (IOException e)
 		{
 			return false;
 		}
+		return true;
 	}
-
 }
