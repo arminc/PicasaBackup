90caf2f34740d4ce2d37a99331b8cd6bdb84697a
Armin <armin@coralic.nl>
2011-08-31 15:28:10 +0200

    FileHandler uses commons-io so no need for testing

--- src/test/java/nl/coralic/picasa/backup/file/FileHandlerTest.java
+++ src/test/java/nl/coralic/picasa/backup/file/FileHandlerTest.java
@@ -8,21 +8,5 @@ import static org.junit.Assert.*;
 
 public class FileHandlerTest
 {
-	@AfterClass
-	public static void tearDown()
-	{
-		
-	}
-	
-	@Test
-	public void folderExists()
-	{
-		assertEquals(true, FileHandler.folderExists("target"));
-	}
-	
-	@Test
-	public void createFolder()
-	{
-		assertEquals(true,FileHandler.createFolder("target","album"));
-	}
+
 }
